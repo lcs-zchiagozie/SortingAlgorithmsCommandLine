df4182ab453205c1c4c72e587d3f28236856f497
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2018-02-26 09:33:31 -0500

    Initial Commit

---
+++ SortingAlgorithmsCommandLine/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SortingAlgorithmsCommandLine
+//
+//  Created by Gordon, Russell on 2018-02-26.
+//  Copyright © 2018 Gordon, Russell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
