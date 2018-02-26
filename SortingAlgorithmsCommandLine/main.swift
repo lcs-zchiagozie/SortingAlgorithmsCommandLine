2bb3797fcb84471c6c98cb2e9eba353f1b8fb484
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2018-02-26 09:41:23 -0500

    Template for command line array sorting program completed

--- SortingAlgorithmsCommandLine/main.swift
+++ SortingAlgorithmsCommandLine/main.swift
@@ -8,5 +8,46 @@
 
 import Foundation
 
-print("Hello, World!")
+// --------------- Convenience functions ---------------
+
+// Generates a random number between the minimum and maximum values given (inclusive)
+func random(between min: UInt32, and max: UInt32) -> Int {
+    return Int(arc4random_uniform(max - min + 1) + min)
+}
+
+// ----------- Start of main part of program -----------
+
+// Ask the user if they are ready to begin
+print("Press any key to generate the unsorted list.", terminator: "")
+
+// Wait for a key to be pressed
+let input = readLine()
+
+// Make an empty array / list
+var numbers : [Int] = []
+
+// Populate the array
+for _ in 1...10 {
+    numbers.append( random(between: 1, and: 50) )
+}
+
+// Show the user the contents of the array
+print("-----")
+print("The contents of the unsorted list:")
+for i in 0...numbers.count - 1 {
+    print("Index \(i), value: \(numbers[i])")
+}
+print("-----")
+
+// ----------- Write code to sort the array here -----------
+print("Now sorting the array...")
+
+// ----------- Final part of the program, show the sorted list -----------
+print("-----")
+print("The contents of the sorted list:")
+for i in 0...numbers.count - 1 {
+    print("Index \(i), value: \(numbers[i])")
+}
+print("-----")
+
 
